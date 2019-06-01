ddf1f486dfd255d0b2bdb4294ac20219d500de75
Mingyu Gao <mingyu.gao96@gmail.com>
2019-06-01 05:31:01 -0700

    add ant design

--- imports/ui/components/App.jsx
+++ imports/ui/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Button from 'antd/lib/button';
 
 const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
+    <Button type="primary">Button</Button>
   </div>
 );
 

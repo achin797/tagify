d0f25bfe8c82d9f2c86e8257c728656a29daf6fd
Mingyu Gao <mingyu.gao96@gmail.com>
2019-06-17 20:36:29 -0700

    add react-router-dom, home page and navbar

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Router, Route, Switch } from 'react-router';
+import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
 import AuthPageSignIn from '../../ui/pages/AuthPageSignIn';
+import HomePage from '../../ui/pages/HomePage';
 import NotFoundPage from '../../ui/pages/NotFoundPage';
 
 const browserHistory = createBrowserHistory();
@@ -11,6 +12,7 @@ export const renderRoutes = () => (
   <Router history={browserHistory}>
     <Switch>
       <Route exact path="/" component={AuthPageSignIn}/>
+      <Route path="/home" component={HomePage}/>
       <Route component={NotFoundPage}/>
     </Switch>
   </Router>

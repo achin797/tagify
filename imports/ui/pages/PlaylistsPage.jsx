f69a818ecdbd910ad550b6225df1a82c0e2f66c5
achin <achintya.chandwani@gmail.com>
2019-07-04 15:02:56 -0700

    Created basic playlists page
    For now it's basically a copy of the songs page

---
+++ imports/ui/pages/PlaylistsPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Layout from 'antd/lib/layout';
+import Divider from 'antd/lib/divider';
+import Typography from 'antd/lib/typography';
+import Navbar from '../components/Navbar';
+import TagsPanel from '../components/TagsPanel';
+import SongList from '../components/SongList';
+
+const { Title } = Typography;
+
+const PlaylistsPage = ({
+                  }) => (
+    <div id="playlists-page">
+        <Layout>
+            <Navbar />
+            <Layout>
+                <TagsPanel />
+                <Divider />
+                <Title>Playlists</Title>
+                <SongList />
+            </Layout>
+        </Layout>
+    </div>
+);
+
+export default PlaylistsPage;

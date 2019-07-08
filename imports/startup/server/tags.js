6a2ae85b5f714671b2c5dae8363b71ce9d75e1e2
Austin <austinkvaale@gmail.com>
2019-07-07 18:00:17 -0700

    Update tags.js

--- imports/startup/server/tags.js
+++ imports/startup/server/tags.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Users = new Mongo.Collection('Users');
 
 Meteor.methods({
@@ -7,7 +6,7 @@ Meteor.methods({
     if (user) {
       return user.tags;
     } else {
-      Users.insert({ 'userSpotifyId': userSpotifyId, tags: [] });
+      Users.insert({ 'userSpotifyId': userSpotifyId, tags: [], taggedSongs: [] });
       return [];
     }
   },
@@ -25,40 +24,19 @@ Meteor.methods({
     Users.update({ 'userSpotifyId': userSpotifyId }, {
       $pull: { tags: { id: tagId } }
     });
-  }
-});
-=======
-userTags = new Mongo.Collection('userTags');
-
-Meteor.methods({
-    
-    getUserTags: function(userSpotifyId) {
-        if(userTags.findOne({"userSpotifyId": userSpotifyId}, {"_id" : 1})){
-            let importedTags = userTags.find(
-                {"userSpotifyId": userSpotifyId}, 
-                {"tags": 1, _id: 0}).fetch();
-            return importedTags[0].tags;
+  },
+  addTaggedSong: (userSpotifyId, tagId, songId) => {
+    const songExists = Users.findOne({ 'userSpotifyId': userSpotifyId }).taggedSongs.findOne({ id: songId });
+    // const song = user.taggedSongs.findOne({ 'id': songId });
+    if (songExists){
+        songExists.update({ 'userSpotifyId': userSpotifyId }, {
+            $push: { tags: tagId }
+        });
+    } else {
+        Users.update({ 
+            'userSpotifyId': userSpotifyId }, {
+            $push: { taggedSongs: { id: songId, tags : [tagId] } }
+            });
         }
-        else{
-            userTags.insert({"tags": []}, {"userSpotifyId": userSpotifyId});
-            return [];
-        }
-    },
-
-    createTag: function(userSpotifyId, displayName) {
-        userTags.update(
-            {'userSpotifyId': userSpotifyId},
-            {$push: {"tags": displayName} }
-        );
-    },
-
-    deleteTag: function(userSpotifyId, tagId) {
-        userTags.update(
-            {'userSpotifyId': userSpotifyId},
-            {$pull: {"tags": tagId} }
-            )
     }
-})
-
-
->>>>>>> 4bfffd2bf3312821520334b0c4ea532424dfe70c
+});

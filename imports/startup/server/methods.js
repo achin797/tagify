f941c62fd3b107241888bc42eef023933c6a546b
achin <achintya.chandwani@gmail.com>
2019-07-29 16:19:02 -0700

    Some problem merging. This commit makes it work

--- imports/startup/server/methods.js
+++ imports/startup/server/methods.js
@@ -6,107 +6,15 @@ Meteor.methods({
       userAvatarUrl: user.profile.images[0].url
     };
   },
-    searchTracks: function(query) {
+  searchTracks: function (query) {
 
-        // Spotify call.
-        var spotifyApi = new SpotifyWebApi();
-        var response = spotifyApi.searchTracks(query, { limit: 50 });
+    // Spotify call.
+    var spotifyApi = new SpotifyWebApi();
+    var response = spotifyApi.searchTracks(query, {limit: 50});
 
-        // Need to refresh token
-        if (checkTokenRefreshed(response, spotifyApi)) {
-            response = spotifyApi.searchTracks(query, { limit: 50 });
-        }
-
-        return response.data.body.tracks.items;
-    },
-
-    //TODO: Add error case handling and better playlist naming
-    createPlaylist: function(playlistName, selectedTracks) {
-        if (selectedTracks.length === 0)
-          throw new Meteor.Error("Cannot generate empty playlist");
-
-        // Call
-        var spotifyApi = new SpotifyWebApi();
-        var response = spotifyApi.createPlaylist(Meteor.user().services.spotify.id, playlistName, { public: true });
-
-        // Need to refresh token
-        if (checkTokenRefreshed(response, spotifyApi)) {
-            response = spotifyApi.createPlaylist(Meteor.user().services.spotify.id, playlistName, { public: true });
-        }
-
-        // Put songs into the playlist.
-        var uris = selectedTracks.map(function(track) {
-            return "spotify:track:"+track.id;
-        });
-
-        response = spotifyApi.addTracksToPlaylist(Meteor.user().services.spotify.id, response.data.body.id, uris, {});
-
-        return response.data.body;
-    },
-
-    getSavedTracks: userId => {
-        var spotifyApi = new SpotifyWebApi();
-
-        var offset = 0;
-        var tracks = [];
-
-        do {
-            var response = spotifyApi.getMySavedTracks({limit:50, offset:offset});
-
-            if (checkTokenRefreshed(response, spotifyApi)) {
-                response = spotifyApi.getMySavedTracks({limit:50, offset:offset});
-            }
-
-            offset += response.data.body.limit;
-            tracks = tracks.concat(response.data.body.items);
-
-        } while(response.data.body.next!=null);
-
-        var updatedTracks = tracks.map(track => {
-            var updated_track = {};
-            updated_track['title'] = track.track.name;
-            updated_track['id'] = track.track.id;
-            updated_track['artists'] = track.track.artists.map(artist => {return artist.name;});
-            updated_track['album'] = track.track.album.name;
-            updated_track['tags'] = [];
-
-            // Grab song tags from DB, if any exist:
-            var currUserDb = Meteor.users.findOne({"_id": userId, "taggedSongs.id": track.track.id });
-            if(currUserDb){
-                tagIdArray = currUserDb.taggedSongs.filter( song => {
-                    if (song.id === track.track.id){
-                        return true;
-                    } else{
-                        return false;
-                    }})[0].tags;
-                updated_track['tags'] = tagIdArray;
-            }
-
-            return updated_track;
-        });
-
-        return updatedTracks;
-    },
-
-    getSavedPlaylists: function() {
-        var spotifyApi = new SpotifyWebApi();
-        var offset = 0;
-        var playlists = [];
-
-        // spotify has an offset and limit. The first call retrieves the first 20 items
-        // then we update the offset and make another call while there are items to fetch
-        do {
-            var response = spotifyApi.getUserPlaylists(Meteor.user().services.spotify.id, {offset: offset});
-
-            if (checkTokenRefreshed(response, spotifyApi)) {
-                response = spotifyApi.getUserPlaylists(Meteor.user().services.spotify.id, {offset: offset});
-            }
-            offset += response.data.body.limit;
-            playlists = playlists.concat(response.data.body.items);
-
-        } while(response.data.body.next!=null);
-
-        return playlists;
+    // Need to refresh token
+    if (checkTokenRefreshed(response, spotifyApi)) {
+      response = spotifyApi.searchTracks(query, {limit: 50});
     }
 
     var tracks = response.data.body.tracks.items;

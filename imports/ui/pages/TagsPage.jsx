c508bca8bc7a810c50c7db52bb1578dfd0471461
Mingyu Gao <mingyu.gao96@gmail.com>
2019-07-29 16:08:35 -0700

    add tagged songs to tags page

--- imports/ui/pages/TagsPage.jsx
+++ imports/ui/pages/TagsPage.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Divider from 'antd/lib/divider';
 import Navbar from '../components/Navbar';
 import Layout from 'antd/lib/layout';
+import List from 'antd/lib/list';
+import Tag from 'antd/lib/tag';
 import Typography from 'antd/lib/typography';
 import notification from 'antd/lib/notification';
 import TagsPanel from '../components/TagsPanel';
@@ -16,9 +19,22 @@ const { Title } = Typography;
 class TagsPage extends Component {
   componentDidMount() {
     this.props.getTags();
+    if (!this.props.hasLoaded) {
+      this.props.getSavedTracks();
+    }
   }
 
   render() {
+    const {
+      hasLoaded,
+      songs,
+      tags
+    } = this.props;
+
+    const dataSource = songs.filter(song => {
+      return song.tags.length > 0;
+    });
+
     return (
       <div id="tags-page">
         <Layout>
@@ -26,6 +42,34 @@ class TagsPage extends Component {
           <Layout>
             <Title>Tags</Title>
             <TagsPanel editable />
+            <Divider />
+            <Title>Tagged Songs</Title>
+            <div id="song-list">
+              <List
+                loading={!hasLoaded}
+                dataSource={dataSource}
+                renderItem={song => (
+                  <List.Item>
+                    <div>
+                      {song.title}
+                    </div>
+                    <div>
+                      <span>
+                        {song.artists.join(', ')} - {song.album}
+                      </span>
+                    </div>
+                    <div>
+                      {song.tags.map((tagId, index) => {
+                        const displayName = tags
+                          .filter(t => t.id === tagId)[0]
+                          .displayName;
+                        return <Tag key={index}>{displayName}</Tag>
+                      })}
+                    </div>
+                  </List.Item>
+                )}
+              />
+            </div>
           </Layout>
         </Layout>
       </div>
@@ -34,7 +78,11 @@ class TagsPage extends Component {
 }
 
 const mapStateToProps = state => {
-  return {};
+  return {
+    hasLoaded: state.songs.hasLoaded,
+    songs: state.songs.songs,
+    tags: state.tags.tags
+  };
 };
 
 const mapDispatchToProps = dispatch => {
@@ -51,6 +99,12 @@ const mapDispatchToProps = dispatch => {
         } else {
           dispatch(getTagsSuccess(response));
         }
+      });
+    },
+    getSavedTracks: () => {
+      Meteor.call('getSavedTracks', Meteor.userId(), (err, response) => {
+        console.log('This message should only display on initial load');
+        dispatch({ type: 'LOAD_SONGS', payload: response });
       });
     }
   };

552985424b6443eb7c4610eeee46506e85aaa271
achin <achintya.chandwani@gmail.com>
2019-08-01 14:49:30 -0700

    Basic rendering of search results implemented

--- imports/ui/components/SearchResults.jsx
+++ imports/ui/components/SearchResults.jsx
@@ -1,34 +1,33 @@
-// import React, {Component} from 'react'
-// import { connect } from 'react-redux';
-// import List from 'antd/lib/list';
-// import TaggableSong from './TaggableSong';
-// import {loadSongs} from "../actions";
-//
-// class SongList extends Component{
-//   constructor(props){
-//     super(props);
-//   }
-//
-//   render(){
-//     const dataSource = this.props.searchResults;
-//
-//     return (
-//       <div id="song-list">
-//         <List
-//           dataSource={dataSource}
-//           renderItem={song => <TaggableSong song={song} />}
-//         />
-//       </div>
-//     );
-//   }
-// }
-//
-// const mapStateToProps = state => {
-//   return {
-//     songs: state.songs.songs,
-//     hasLoaded: state.songs.hasLoaded,
-//     checkedTags: state.tagsPanel.checkedTags,
-//   };
-// };
-//
-// export default connect(mapStateToProps, {loadSongs})(SearchResults);
+import React, {Component} from 'react'
+import { connect } from 'react-redux';
+import List from 'antd/lib/list';
+import TaggableSong from './TaggableSong';
+
+class SearchResults extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    const dataSource = this.props.searchResults;
+
+    return (
+      <div id="search-results">
+        <List
+          loading={!this.props.hasLoaded}
+          dataSource={dataSource}
+          renderItem={song => <TaggableSong song={song} />}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    searchResults: state.searchResults.songs,
+    hasLoaded: state.searchResults.hasLoaded,
+  };
+};
+
+export default connect(mapStateToProps, {})(SearchResults);

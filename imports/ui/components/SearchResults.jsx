86c3b71754596070aa6be400e753834b991d2425
achin <achintya.chandwani@gmail.com>
2019-08-03 11:34:11 -0700

    Added conditional rendering of list in search page

--- imports/ui/components/SearchResults.jsx
+++ imports/ui/components/SearchResults.jsx
@@ -14,7 +14,6 @@ class SearchResults extends Component{
     return (
       <div id="search-results">
         <List
-          loading={!this.props.hasLoaded}
           dataSource={dataSource}
           renderItem={song => <TaggableSong song={song} isSearchedSong={true}/>}
         />
@@ -26,7 +25,6 @@ class SearchResults extends Component{
 const mapStateToProps = state => {
   return {
     searchResults: state.searchResults.songs,
-    hasLoaded: state.searchResults.hasLoaded,
   };
 };
 
